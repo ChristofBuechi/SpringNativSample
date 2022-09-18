14fd4395facef420be6c925d0346a06d56eb1300
Christof <christof.buechi@gmail.com>
2022-09-18 16:02:33 +0200

    add logging

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -3,6 +3,8 @@ package com.example.restservice;
 import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+
+	private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
@@ -20,6 +25,8 @@ public class GreetingController {
 
 	@GetMapping("/time")
 	public LocalDateTime actualTime() {
-		return LocalDateTime.now();
+		LocalDateTime now = LocalDateTime.now();
+		log.info("Actual Time: {}", now);
+		return now;
 	}
 }
